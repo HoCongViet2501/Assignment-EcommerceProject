80a202da2416b95d2d6a2dc758c606b8046d8613
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-06-16 20:50:35 +0700

    create and add back-end project

---
+++ Assignment-EcommerceProject-BackEnd/src/main/java/com/assignment/springboot/AssignmentEcommerceProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentEcommerceProjectBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentEcommerceProjectBackEndApplication.class, args);
+    }
+
+}
